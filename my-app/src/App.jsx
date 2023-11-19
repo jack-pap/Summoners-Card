318077e22efa0329ec2e33caccd74521d7c0f619
Jack Papaioannou <jackpapaioannou1@gmail.com>
2023-11-19 15:46:39 +0000

    beginner files

---
+++ my-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Summoners Card</h1>
+      <input type="text" name="summoner" placeholder='Enter summoner name' />
+      <div className="box">
+        <select name="server">
+          <option value="euw">EUW</option>
+          <option value="eune">EUNE</option>
+          <option value="na">NA</option>
+        </select>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Search
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
