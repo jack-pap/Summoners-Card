16aa6c0e36265edbe5100f6f186ed73445e4c25b
Jack Papaioannou <jackpapaioannou1@gmail.com>
2023-11-21 19:16:41 +0000

    Method for input grab on button click, css styling

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,19 +7,32 @@ function App() {
   return (
     <>
       <h1>SUMMONERS CARD</h1>
-      <input type="text" name="summoner" placeholder='Enter summoner name...' />
+      <input type="text" className="summonerField" id="summoner" placeholder='Enter summoner name...' />
       <div className="box">
-        <select name="server">
-          <option value="euw">EUW</option>
-          <option value="eune">EUNE</option>
-          <option value="na">NA</option>
+        <select id="server">
+          <option value="EUW">EUW</option>
+          <option value="EUNE">EUNE</option>
+          <option value="NA">NA</option>
         </select>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={getInput}>
           Search
         </button>
       </div>
     </>
   )
 }
+
+function getInput() {
+  const summoner = document.getElementById("summoner").value;
+  const server = document.getElementById("server").value;
+  if (summoner.match(/^[0-9a-z]+$/) && server) {
+    document.getElementById("summoner").value = "";
+    /*alert(summoner + " " + server)
+    document.getElementById("rightLine").classList.add('horizMoveLeft');
+    document.getElementById("leftLine").classList.add('horizMoveRight');*/
+    document.getElementById("root").classList.add('dissapear');
+  }
+}
+
 
 export default App
