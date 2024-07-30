36c1ebbe2c5b1be04292adc61a01958c3c3a6eea
Jack Papaioannou <62362994+jack-pap@users.noreply.github.com>
2024-07-30 17:53:02 +0300

    Removed workflow for Node.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
   plugins: [react()],
 })
