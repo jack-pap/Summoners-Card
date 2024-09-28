4aa3858b483fcba8599717e488910f1400279f95
Jack Papaioannou <62362994+jack-pap@users.noreply.github.com>
2024-09-28 16:05:33 +0300

    Documentation work for docsify and changed port 3000 to 3001

--- my-app/vite.config.mjs
+++ my-app/vite.config.mjs
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/assets': 'http://localhost:3000'
+      '/assets': 'http://localhost:3001'
     }
   },
   esbuild: {
