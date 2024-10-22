f25eae2030424c8962657d923ae872110fd050de
Jack Papaioannou <62362994+jack-pap@users.noreply.github.com>
2024-10-22 14:36:16 +0300

    Added missed new export to a route

--- src/app/api/summoners/[puuid]/route.js
+++ src/app/api/summoners/[puuid]/route.js
@@ -1,12 +1,19 @@
+import { getDB } from "@/src/utils/db";
 import { NextResponse } from "next/server";
-import db from '@/src/utils/db'
 
 export async function GET(request, { params }) {
-    const { puuid } = params;
-    try {
-      const userData = await db.select('summonerInfo').from('summonerInfo').where('puuid', puuid);
-      return NextResponse.json(userData);
-    } catch (error) {
-      return NextResponse.json({ message: `There was an error retrieving summoner info: ${error}` }, { status: 500 });
-    }
+  const { puuid } = params;
+  try {
+    const db = getDB();
+    const userData = await db
+      .select("summonerInfo")
+      .from("summonerInfo")
+      .where("puuid", puuid);
+    return NextResponse.json(userData);
+  } catch (error) {
+    return NextResponse.json(
+      { message: `There was an error retrieving summoner info: ${error}` },
+      { status: 500 }
+    );
   }
+}
