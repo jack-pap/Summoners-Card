3bc37fcc57ceca358c56df4c3387a65c608e38ba
Jack Papaioannou <62362994+jack-pap@users.noreply.github.com>
2024-10-24 00:52:53 +0300

    Test

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -11,55 +11,27 @@ const port = 3001;
 const NodeCache = require("node-cache");
 const cache = new NodeCache({ stdTTL: 10000, checkperiod: 120 });
 
-// const summonerRouter = require("./summoner-db-route");
-// const matchRouter = require("./match-db-route");
-
-// Middleware to cache responses
-// const cacheMiddleware = (req, res, next) => {
-//   const key = req.query.url;
-//   const cachedResponse = cache.get(key);
-
-//   if (cachedResponse) {
-//     console.log(`Cache hit for URL: ${key}`);
-//     res.json(cachedResponse);
-//   } else {
-//     console.log(`Cache miss for URL: ${key}`);
-//     res.sendResponse = res.json;
-//     res.json = (body) => {
-//       cache.set(key, body);
-//       res.sendResponse(body);
-//     };
-//     next();
-//   }
-// };
-
 app.prepare().then(() => {
   const server = express();
 
-  // Custom route example
   server.get("/p/:id", (req, res) => {
     const actualPage = "/post";
     const queryParams = { id: req.params.id };
     app.render(req, res, actualPage, queryParams);
   });
 
-  server.use(cors()); // Enable CORS for everywhere
-  server.use(bodyParser.json()); // Parse JSON bodies
+  server.use(cors()); 
+  server.use(bodyParser.json()); 
   server.use((req, res, next) => {
     if (req.method === "OPTIONS") {
       res.setHeader("Access-Control-Allow-Origin", req.get("Origin") || "*");
       res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-      // Allow private network access
       res.setHeader("Access-Control-Allow-Private-Network", "true");
       return res.status(200).end();
     }
     next();
   });
-
-  //Database middleware routes
-  // app.use("/summoner", summonerRouter);
-  // app.use("/match", matchRouter);
 
   server.get("/", (req, res) => {
     res.send("Welcome to Summoners Card!");
