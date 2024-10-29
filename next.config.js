c8310ed5092eafa093357d39092f37272b3d6213
Jack Papaioannou <62362994+jack-pap@users.noreply.github.com>
2024-10-29 14:16:24 +0200

    Using SWC

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ import path from "path";
 import webpack from "webpack";
 
 export default {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  compiler: {
+    // This will enable both styled-components and styled-jsx
+    styledComponents: true,
+    emotion: true,
+  },
   webpack: (config, { isServer }) => {
     // Add alias for @
     config.resolve.alias["@"] = path.resolve();
