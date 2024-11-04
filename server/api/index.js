50687993f1dc271c5fda919d0c93f50e1c89d386
Jack Papaioannou <62362994+jack-pap@users.noreply.github.com>
2024-11-04 20:04:58 +0200

    Caching on backend aswell

--- server/api/index.js
+++ server/api/index.js
@@ -19,12 +19,16 @@ const cacheMiddleware = (req, res, next) => {
 
   if (cachedResponse) {
     console.log(`Cache hit for URL: ${key}`);
+    res.set("Cache-Control", "public, max-age=36000, immutable");
+    res.set("Expires", new Date(Date.now() + 36000).toUTCString());
     res.json(cachedResponse);
   } else {
     console.log(`Cache miss for URL: ${key}`);
     res.sendResponse = res.json;
     res.json = (body) => {
       cache.set(key, body);
+      res.set("Cache-Control", "public, max-age=36000, immutable");
+      res.set("Expires", new Date(Date.now() + 36000).toUTCString());
       res.sendResponse(body);
     };
     next();
